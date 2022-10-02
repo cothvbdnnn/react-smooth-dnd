8bd1cce74a70867ec0442fa24ebbafa55743c7cb
kuu <cothvbdnnnn@gmail.com>
2022-10-02 16:52:33 +0700

    fix: not assignable to type undefined

--- src/Container.tsx
+++ src/Container.tsx
@@ -121,10 +121,9 @@ class Container extends Component<PropsWithChildren<ContainerProps>> {
   }
 
   getContainerOptions(): ContainerOptions {
-    return Object.keys(this.props).reduce((result: ContainerOptions, key: string) => {
+    return Object.keys(this.props).reduce((result: any, key: string) => {
       const optionName = key as keyof ContainerOptions;
       const prop = this.props[optionName];
-
       if (typeof prop === 'function') {
         result[optionName] = (...params: any[]) => {
           return (this.props[optionName] as Function)(...params);
